d60f9239b8ee9ee818deaa992dda9e50fb75a40e
andiarioo <arioandiid2812@gmail.com>
2021-03-24 22:33:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pertemuan5/ForgotPassword.java
@@ -0,0 +1,47 @@
+package com.example.pertemuan5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private Button resetPasswordButton;
+
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        emailEditText = (EditText) findViewById(R.id.email);
+        resetPasswordButton = (Button) findViewById(R.id.resetPassword);
+
+        auth = FirebaseAuth.getInstance();
+
+        resetPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetPassword();
+            }
+        });
+    }
+
+    private void resetPassword() {
+        String email = emailEditText.getText().toString().trim();
+
+        if(email.isEmpty()) {
+            emailEditText.setError("Email is required!");
+            emailEditText.requestFocus();
+            return;
+        }
+
+    }
+}
